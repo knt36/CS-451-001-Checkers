f35eb3c7a6cccdfa241ac167fba1280357135c07
Khoi Tran <knt36@drexel.edu>
2016-07-23 14:36:03 -0400

    UX Skeleton

---
+++ src/ux/Buttons/ButtonFactory.java
@@ -0,0 +1,7 @@
+package Buttons;
+
+import java.awt.Color;
+
+public abstract class ButtonFactory {
+	protected abstract Button genButton(Color c, String s);
+}
---
+++ src/ux/Buttons/Disk.java
@@ -0,0 +1,8 @@
+package Buttons;
+
+import java.awt.Color;
+
+public class Disk {
+	protected Color c = null;
+	protected boolean king = false;
+}
