afeee07d3d0afe9cec5703c9598f5e2687f4b2da
Maxbot100 <mm3888@drexel.edu>
2016-07-23 10:56:57 -0400

    Adding initial classes & testing Intellij integration

---
+++ src/game/Disk.java
@@ -0,0 +1,9 @@
+package game;
+
+/**
+ * Created by maxm on 7/21/2016.
+ */
+public class Disk {
+    public Color color;
+    public Boolean king;
+}
---
+++ src/game/NullDisk.java
@@ -0,0 +1,7 @@
+package game;
+
+/**
+ * Created by maxm on 7/21/2016.
+ */
+public class NullDisk extends Disk {
+}
