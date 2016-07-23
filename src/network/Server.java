6b29f6d7a5c9b8be702dcbbe8a97ca79476d6161
Maxbot100 <mm3888@drexel.edu>
2016-07-23 16:58:10 -0400

    Added remaning UML classes

---
+++ src/network/Server.java
@@ -0,0 +1,29 @@
+package network;
+
+import java.net.Socket;
+
+/**
+ *
+ */
+public class Server {
+    private Utils utils = new Utils();
+
+    public String generateToken() {
+        return null;
+    }
+
+    public Socket createSSLSocket() {
+        return null;
+    }
+
+    private void createUserRecord(String username, String token) {
+    }
+
+    private Boolean destroyToken(String token) {
+        return false;
+    }
+
+    private Boolean pruneTokens() {
+        return false;
+    }
+}
