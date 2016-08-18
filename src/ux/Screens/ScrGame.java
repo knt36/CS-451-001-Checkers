c339cfb8d2b9ee45ff6321223dcbed326ce6b35e
Khoi Tran <knt36@drexel.edu>
2016-08-18 18:16:17 -0400

    Remove text test

--- src/ux/Screens/ScrGame.java
+++ src/ux/Screens/ScrGame.java
@@ -1,8 +1,10 @@
 package ux.Screens;
 
+import static game.Color.*;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.Ellipse2D;
 import java.util.Date;
 import java.util.Timer;
 
@@ -13,11 +15,13 @@ import javax.swing.JPanel;
 
 import game.Game;
 import game.MoveStatus;
+import javafx.scene.shape.Circle;
 import ux.Buttons.GuiBoard;
 import ux.Buttons.ListenerBoard;
 import ux.Buttons.OptionButton;
 import ux.Labels.HeaderLabel;
 import ux.Labels.NoteLabel;
+
 
 public class ScrGame extends ScrFactory{
 	protected GuiBoard board = null;
@@ -27,16 +31,19 @@ public class ScrGame extends ScrFactory{
 	protected String turnIndicator = "'s Turn! ";
 	protected NoteLabel playerTurn = new NoteLabel("");
 	protected Game game = null;
+	protected OptionButton turnColorIndicator = new OptionButton(Color.red, "Color");
 	
 	public ScrGame(Game game) {
 		// TODO Auto-generated constructor stub
 		this.game = game;
 		this.board = new GuiBoard(this.game);
-		this.constr.gridheight=4;
+		this.constr.gridheight=5;
 		this.add(this.board);
 		this.constr.gridheight=1;
 		this.constr.gridx++;
 		this.constr.fill=this.constr.NONE;
+		this.add(this.turnColorIndicator);
+		this.constr.gridy++;
 		this.add(this.playerTurn);
 		this.constr.gridy++;
 		this.add(helpBt);
@@ -170,9 +177,15 @@ public class ScrGame extends ScrFactory{
 	}
 	public void setTurnText(){
 		this.playerTurn.setText(this.game.turn.getName()+this.turnIndicator);
-	}
-	
-	public void setPieces(){
-	
+		Color c = null;
+		
+		if(this.game.turn.getColor() == RED){
+			this.turnColorIndicator.setBackground(Color.red);
+			this.turnColorIndicator.setForeground(Color.black);
+		}else if (this.game.turn.getColor() == WHITE){
+			this.turnColorIndicator.setBackground(Color.white);
+			this.turnColorIndicator.setForeground(Color.black);
+		}
+		
 	}
 }
