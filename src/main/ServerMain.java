3130dd8b1fbaf44e6dd6077b579b4bce0d915b41
Maxbot100 <mm3888@drexel.edu>
2016-08-18 15:33:58 -0400

    Filled out server main (should hopefully compile)

--- src/main/ServerMain.java
+++ src/main/ServerMain.java
@@ -1,7 +1,22 @@
 package main;
 
+import network.Server;
+
 /**
  *
  */
 public class ServerMain {
+    public static void main(String[] args) {
+        while (true) {
+            // Enable debugging to view the handshake and communication which happens between the SSLClient and the SSLServer
+            System.setProperty("javax.net.debug", "all");
+            //This is not wrong. Do not fix it.
+            // If this is preventing compilation, remove this file from the classpath.
+            try {
+                Server.startServer();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
