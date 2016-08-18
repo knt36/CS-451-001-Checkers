0f69e1db82193a4478c19511fc6ef1e5eb0db3cb
Mark Blaho <cpb49@drexel.edu>
2016-08-18 19:39:51 -0400

    Initial Salt generation

--- src/network/Utils.java
+++ src/network/Utils.java
@@ -1,15 +1,29 @@
 package network;
 
+import java.nio.charset.Charset;
+import java.security.SecureRandom;
+import java.util.Random;
+
 /**
  *
  */
 public class Utils {
+    public static final int ITERATIONS = 100;
+    public static final int KEY_LENGTH = 256;
     public Boolean send(Object data) {
         return false;
     }
 
-    private String hash(String str) {
-        return null;
+    public String generateSalt() {
+        final Random r = new SecureRandom();
+        byte[] salt = new byte[32];
+        r.nextBytes(salt);
+        return new String(salt, Charset.defaultCharset() );
+    }
+
+    private String hash(String password, String salt, int iterations, int key_length) {
+
+
     }
 
     public Boolean verifyHash(String hash, String data) {
