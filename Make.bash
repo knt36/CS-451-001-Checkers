a1b3061b434e2c774c141cb3a8d44a875544a658
Max M <maxbot100@yahoo.com>
2016-08-20 09:17:06 -0400

    Added more print statements

--- Make.bash
+++ Make.bash
@@ -2,13 +2,13 @@
 
 JC="javac"
 JVM="java"
-if [ "$1" = "server" ]
-    then
-        "$JC" -cp lib/gson-2.7.jar:lib/mysql-connector-java-5.0.8-bin.jar:src -d out src/main/ServerMain.java
-        "$JVM" -cp lib/gson-2.7.jar:lib/mysql-connector-java-5.0.8-bin.jar:src:out main.ServerMain
-elif [ "$1" == "client" ]
-    then
+if [ "$1" = "server" ]; then
+    "$JC" -cp lib/gson-2.7.jar:lib/mysql-connector-java-5.0.8-bin.jar:src -d out src/main/ServerMain.java
+    echo "Starting server"
+    "$JVM" -cp lib/gson-2.7.jar:lib/mysql-connector-java-5.0.8-bin.jar:src:out main.ServerMain
+elif [ "$1" == "client" ]; then
     "$JC" -cp lib/gson-2.7.jar:lib/mysql-connector-java-5.0.8-bin.jar:src -d out src/main/ClientMain.java
+    echo "Starting client"
     "$JVM" -cp lib/gson-2.7.jar:lib/mysql-connector-java-5.0.8-bin.jar:src:out main.ClientMain
 else
     echo "Call this with client or server!"
